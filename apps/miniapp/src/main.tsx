70d384a9a46f1b43db1e9485a89769539ed26d07
tracerxbrhd <99992960+tracerxbrhd@users.noreply.github.com>
2026-09-16 17:13:03 +0200

    style(client): load tactile motion layer

--- apps/miniapp/src/main.tsx
+++ apps/miniapp/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App.js';
 import './styles.css';
 import './table.css';
+import './motion.css';
 import './rules.css';
 import './result.css';
 
